1e6cf958d10413d2d3633cdec3eed850b16aec4b
dydyandra <dy.dyandraparamitha@gmail.com>
2021-03-26 09:15:31 +0700

    commit no 2 lagi

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 #SOAL A
-export LC_ALL=C
+LC_ALL=C \
 awk -F"\t" '
 BEGIN{}
 {percentage=$21/($18-$21)*100}
-{if (NR>1 && max<=percentage){
+{if (NR !=1 && max<=percentage){
     max=percentage
     ID=$1}
 }
-
 END{
 print "Transaksi terakhir dengan profit percentage terbesar yaitu", ID, "dengan persentase", max,"%\n"}
 ' /home/dyandra/praktikum1/Laporan-TokoShiSop.tsv > hasil.txt #untuk soal E
 
 
 #SOAL B
-export LC_ALL=
+LC_ALL= \
 awk -F"\t" \
 -v year="2017" \
 -v city="Albuquerque" '
 BEGIN{}
 
+{if (NR!=1){
 {if ($2~year){
- {if ($10==city){names[$7]++}}
-}}
+ {if ($10==city)
+ {names[c]=$7; c++}}
+}}}}
 
 END {
 print "\nDaftar nama customer di",city,"pada tahun",year,"antara lain:"
-{for (name in names){
-   print name
+{for (i=1; i<=c; i++){
+   if (names[i]!=names[i+1]){
+   print names[i]}
 }}}
 ' /home/dyandra/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt #untuk soal E
 
@@ -47,7 +49,7 @@ END{
 {for (i in a){
     if (min > a[i]){
 	min = a[i]
-        min = i}
+	min = i}
 }}
 
 
@@ -55,19 +57,20 @@ print "\nTipe segmen customer yang penjualannya paling sedikit adalah", min, "de
 ' /home/dyandra/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt  #untuk soal E
 
 #SOAL D
-export LC_ALL=C
+LC_ALL=C \
 awk -F"\t" \
 -v min=999999 '
 BEGIN{}
 
-{if (NR!=1){a[$13]+=$21}}
+{if (NR!=1)
+{a[$13]+=$21}}
 
 END{
 
 {for (i in a){
     if (min > a[i]){
         min = a[i]
-	min = i}
+        min = i}
 }}
 print "\nWilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah", min, "dengan total keuntungan", a[min], "\n"}
 ' /home/dyandra/praktikum1/Laporan-TokoShiSop.tsv >> hasil.txt  #untuk soal E
