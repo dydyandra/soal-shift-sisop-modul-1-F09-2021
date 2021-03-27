70d57153b72f96420dc7d0a4ba55629beecf7d9f
muthiaqrrta <68548653+muthiaqrrta@users.noreply.github.com>
2021-03-27 15:00:11 +0700

    3a dan 3b

---
+++ soal3/soal3a.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+for ((i=1; i<=23; i++)); 
+do
+	wget -a Foto.log -O "Koleksi_$i.jpg" https://loremflickr.com/320/240/kitten >> a.log
+
+        for ((j=1; j<i; j++));
+        do
+                if diff Koleksi_$j.jpg Koleksi_$i.jpg &> /dev/null;
+                then 
+                        rm Koleksi_$i.jpg
+			break;
+                fi
+        done
+done
+
+for ((i=1; i<=23; i++)); 
+do
+    if [ ! -f Koleksi_$i.jpg ]; then
+        for ((j = 23; i < j; j--)); do
+            if [ -f Koleksi_$j.jpg ]; then
+                mv Koleksi_$j.jpg Koleksi_$i.jpg
+                break
+            fi
+        done
+    fi
+done
+
+for i in {1..9}; do
+        mv Koleksi_$i.jpg Koleksi_0$i.jpg
+done
+
