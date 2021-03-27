a07d538baaf1542a363502ffbc9200ca09541b3b
muthiaqrrta <68548653+muthiaqrrta@users.noreply.github.com>
2021-03-27 18:35:17 +0700

    3c

---
+++ soal3/soal3c.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+countKucing=$(find Kucing_* -type f | wc -l)
+countKelinci=$(find Kelinci_* -type f | wc -l)
+
+if (( countKucing == countKelinci ));
+then
+	namaFolder="Kucing_$(date +%d)-$(date +%m)-$(date +%Y)"
+	mkdir $namaFolder
+
+	for ((i=1; i<=23; i++));
+	do
+        	wget -a Foto.log -O "Koleksi_$i.jpg" https://loremflickr.com/320/240/kitten >> a.log
+        	for ((j=1; j<i; j++));
+        	do
+                	if diff Koleksi_$j.jpg Koleksi_$i.jpg &> /dev/null;
+                	then
+	                        rm Koleksi_$i.jpg
+				break;
+                	fi
+	        done
+	done
+
+	for ((i=1; i<=23; i++)); 
+	do
+	    if [ ! -f Koleksi_$i.jpg ]; then
+        	for ((j = 23; i < j; j--)); do
+	            if [ -f Koleksi_$j.jpg ]; then
+	                mv Koleksi_$j.jpg Koleksi_$i.jpg
+	                break
+	            fi
+        	done
+	    fi
+	done
+
+	for i in {1..9}; do
+        	mv Koleksi_$i.jpg Koleksi_0$i.jpg
+	done
+
+	mv *.jpg $namaFolder
+	mv Foto.log $namaFolder
+
+elif ((countKucing > countKelinci))
+then 
+	namaFolder="Kelinci_$(date +%d)-$(date +%m)-$(date +%Y)"
+        mkdir $namaFolder
+
+        for ((i=1; i<=23; i++));
+        do
+                wget -a Foto.log -O "Koleksi_$i.jpg" https://loremflickr.com/320/240/bunny >> a.log3
+                for ((j=1; j<i; j++));
+                do
+                        if diff Koleksi_$j.jpg Koleksi_$i.jpg &> /dev/null;
+                        then
+                                rm Koleksi_$i.jpg
+                                break;
+                        fi
+                done
+        done
+
+        for ((i=1; i<=23; i++)); 
+        do
+            if [ ! -f Koleksi_$i.jpg ]; then
+                for ((j = 23; i < j; j--)); do
+                    if [ -f Koleksi_$j.jpg ]; then
+                        mv Koleksi_$j.jpg Koleksi_$i.jpg
+                        break
+                    fi
+                done
+            fi
+        done
+
+        for i in {1..9}; do
+                mv Koleksi_$i.jpg Koleksi_0$i.jpg
+        done
+
+        mv *.jpg $namaFolder
+        mv Foto.log $namaFolder
+
+fi
